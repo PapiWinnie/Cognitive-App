e539edd7fd952af4ff581bfde80b1a2d242f5a5a
Emma-Asoliya <e.briggs@alustudent.com>
2024-07-03 15:30:49 +0200

    makinmg of symbols

--- Cognitive-health.py
+++ Cognitive-health.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 #List of symbols for the cards
-symbols = [
+symbols = ['❤','🤩','🤑','🤡','😎','😂','😏','🥵']
+symbols = symbols * 2 #to make pairs
 
+#to shuffle the symbols randomly
+random.shuffle(symbols)
