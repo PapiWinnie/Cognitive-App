91c66a7821a0c5288e287af7baf13862b9732b81
Emma-Asoliya <e.briggs@alustudent.com>
2024-07-04 13:51:19 +0200

    update

--- Cognitive-health.py
+++ Cognitive-health.py
@@ -6,3 +6,13 @@ symbols = symbols * 2 #to make pairs
 
 #to shuffle the symbols randomly
 random.shuffle(symbols)
+
+#List of numbers for the cards
+numbers = ['2','4','6','8','10','12','14','16']
+numbers =numbers * 2 #to make pairs
+random.shuffle(numbers)
+
+#List of letters for cards 
+letters = ['A','F','H','U','L','T','C','Z']
+letters = letters * 2 #to make pairs
+random.shuffle(letters)
